a2902d8f6717ac441ded75814742b0cf9c95773b
jackamo <jackrob1994@gmail.com>
2016-03-04 15:53:20 +1300

    Refactored the timer code

--- timer.js
+++ timer.js
@@ -14,30 +14,34 @@ var total = 7;
 function getNextHandInDate(){
 	var value = 0;
 	handins.forEach(function(x){
-	if(Date.now() > x[1]){
-	value += 1;
+		if(Date.now() > x[1]){
+			value += 1;
 		} 
 	});
 	return value;
 }
 
 function renderHandins(){
-	// Now, I know I'm injecting HTML elements here, but I technical debt is something for future me.
-
 	$(".hand-ins").empty();
 	var ratio = 1.6;
 	var maxSize = 80;
 	var currentHandin = getNextHandInDate();
 				
 	var getSize = function(i){
-		return i == currentHandin ?  maxSize : maxSize / ((i - currentHandin) * ratio);
+		return i == currentHandin 
+		? maxSize 
+		: maxSize / ((i - currentHandin) * ratio);
 	}
 
 	var getOpacity = function(i){
-		return i == currentHandin ?  1 - (1 / maxSize) * 10 : 1 - (1 / (maxSize / ((i - currentHandin) * ratio))) * 10 < 0.1 ? "0.0; display: none;" : i == currentHandin ?  1 - (1 / maxSize) * 10 : 1 - (1 / (maxSize / ((i - currentHandin) * ratio))) * 10; 
+		var opacity = i == currentHandin 
+			?  1 - (1 / maxSize) * 10 
+			: 1 - (1 / (maxSize / ((i - currentHandin) * ratio))) * 10;
+
+		return opacity;
 	}
 
-	var getColor = function(date, i){
+	var getColor = function(date){
 		if(date["days"] <= 30){
 			return "bad";
 		}
@@ -48,13 +52,47 @@ function renderHandins(){
 		}
 	}
 
-	var buildString = function(date, i){
-		return "<span class='date " + getColor(date, i) + "'>" + date["days"] + " DAYS, " + date["hours"] + ":" + date["minutes"] + ":" + date["seconds"] + "</span>";
+	var buildString = function(date){
+		return date["days"] + " DAYS, " + date["hours"] + ":" + date["minutes"] + ":" + date["seconds"];
 	}
 
 	for(var i = handins.length - 1; i >= currentHandin; i--){
-		$(".hand-ins").prepend('<p style="font-size: ' +  getSize(i) + '; opacity: ' + getOpacity(i) + '">' + handins[i][0].toUpperCase()  +'</p>' +
-			'<p style="font-size: ' +  getSize(i) + '; opacity: ' + getOpacity(i) + '; padding-top: 0;">' + buildString(timeUntil(handins[i][1]), i) + '</p>');
+		
+		var fontSize = getSize(i);
+		var opacity = getOpacity(i);
+		var handInDate = handins[i][1];
+
+		var handIn = $("<p></p>");
+		handIn.css("font-size", fontSize);
+
+		var time = $("<p></p>");
+		time.css("font-size", fontSize);
+		time.css("padding-top", 0);
+
+		if(opacity < 0.1){
+			handIn.css("display", "none");
+			time.css("display", "none");
+
+		}else{
+			handIn.css("opacity", opacity);
+			time.css("opacity", opacity);
+		}
+
+		handIn.html(handins[i][0].toUpperCase());
+
+		var span = $("<span></span");
+		var timeLeft = timeUntil(handInDate);
+		
+		span.addClass("date");
+		span.addClass(getColor(timeLeft));
+
+		span.html(buildString(timeLeft));
+
+
+		time.html(span);
+
+		$(".hand-ins").prepend(time);
+		$(".hand-ins").prepend(handIn);
 	}
 }
 
